ae249a523a67db4f763af33a8f2616965df27581
Rodrigo Luzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2015-04-30 22:17:13 -0700

    no message

---
+++ header.php
@@ -0,0 +1,61 @@
+
+<html>
+<head>
+  <title>Dunder Mifflin Paper Products</title>
+<!-- Define Style in document -->  
+<style type="text/css">
+    h1 {color:white; font-size:24pt; text-align:center;
+        font-family:arial,sans-serif}
+    .menu {color:white; font-size:12pt; text-align:center;
+           font-family:arial,sans-serif; font-weight:bold}
+    p {color:black; font-size:12pt; text-align:justify;
+       font-family:arial,sans-serif}
+    p.foot {color:white; font-size:9pt; text-align:center;
+            font-family:arial,sans-serif; font-weight:bold}
+    a:link,a:visited,a:active {color:white}
+  </style>
+</head>
+<body>
+
+  <!-- page header -->
+  <table width="100%" cellpadding="12" cellspacing="0" border="0">
+  <tr bgcolor="#15384E">
+    <td align="left"><img src="logo.jpg" alt="TLA logo" height="70" width="70"></td>
+    <td>
+        <h1>Dunder Mifflin Paper Products</h1>
+    </td>
+    <td align="right"><img src="logo.jpg" alt="TLA logo" height="70" width="70" /></td>
+  </tr>
+  </table>
+
+  <!-- menu -->
+
+<!-- Label and link to Home -->
+  <table width="100%" bgcolor="white" cellpadding="4" cellspacing="4">
+  <tr bgcolor="#15384E">
+    <td width="25%" href="home.php" style="cursor:pointer">
+      <a href="home.php">
+      <img src="s-logo.png" alt="" height="20" width="20" />
+      <span class="menu">Home</span></a></td>
+
+<!-- Label and link to Products -->
+    <td width="25%" href="products.php" style="cursor:pointer">
+      <a href="products.php">
+      <img src="s-logo.png" alt="" height="20" width="20" />
+      <span class="menu">Products</span></a></td>
+
+<!-- Label and link to Orderform.html -->
+    <td width="25%" href="orderform.html" style="cursor:pointer">
+      <a href="orderform.html">
+      <img src="s-logo.png" alt="" height="20" width="20" />
+      <span class="menu">Order</span></a></td>
+
+
+    <td width="25%">
+      <img src="s-logo.png" alt="" height="20" width="20" />
+      <span class="menu">Feedback</span></td>
+
+
+  </tr>
+  </table>
+
