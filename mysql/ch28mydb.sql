7d3378d1836280742e1f557a5a0083e2a390aee3
TophSmash <heatranoveryou@yahoo.com>
2015-05-25 06:03:55 -0700

    Updated database files to match ch28's
    Needed a new file because I can't change Melissa's users table.

--- mysql/ch28mydb.sql
+++ mysql/ch28mydb.sql
@@ -3,14 +3,14 @@ create database dunder_mifflin;
 use dunder_mifflin;
 
 create table customers
-( customerid varchar(60) not null primary key,
-  passwd varchar(20) not null,
-  email_address varchar(60) not null,
-  name char(50) not null,
-  address char(100) not null,
+( 
+  customerid int unsigned not null auto_increment primary key,
+  name char(60) not null,
+  address char(80) not null,
   city char(30) not null,
-  state char(2) not null,
-  tel_number char(15) not null
+  state char(20),
+  zip char(10),
+  country char(20) not null
 );
 
 create table orders
@@ -34,8 +34,8 @@ create table categories
 );
 
 create table order_items
-( item_num varchar(13) not null,
-  orderid int unsigned not null,
+( orderid int unsigned not null,
+  item_num varchar(13) not null,
   item_price float(4,2) not null,
   quantity smallint unsigned,
   primary key (orderid, item_num)
