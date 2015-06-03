903accaa08520c1710f592c03337527e746e788b
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2015-06-02 22:49:43 -0700

    Edited footer so that the bottom links are simetrical. Also tried to incorporate bootstrap but there is a lot of css already on the site and I would have to change too much.

--- htdocs_loginworking05272015/footer.php
+++ htdocs_loginworking05272015/footer.php
@@ -4,27 +4,29 @@
 //Date: 05/27/2015
 //Project Description: -->
 <!-- General Company Info -->
-  <table style="float: left;" width="100%" bgcolor="black" cellpadding="12" border="0">
+  <table style="float: left;" width="100%" bgcolor="black" cellpadding="13" border="0">
   <tr>
     <td>
       <p class="foot">&copy; Dunder Mifflin Paper Products</p>
       <p class="foot">1725 Slough Avenue in Scranton, PA</p>
       <p class="foot"> 1-800-984-3672</p>
-    </td>
-    </table>
+    </td> 
+    </table> 
+
+
     <!-- Begin Link Bar -->   
-    <table style="float: left;" width="100%" bgcolor="black" cellpadding="20" border="0">
+    <table style="float: left;" width="100%" bgcolor="black" cellpadding="13" border="0">
       <p class="foot">
     <!--Terms of Service -->
     <td style="text-align:center;">
     <a href="tos.php">
       <span class="menu">Terms Of Service</span></td>
     <!--Feedback -->
-    <td style="text-align:center;">
+    <td style="text-align:justify;">
     <a href="Feedback.php">
       <span class="menu">Feedback</span></td>
     <!--About.php -->
-    <td style="text-align:center;">
+    <td style="text-align:left;">
     <a href="about.php">
       <span class="menu">About</span></td>
     </p>  
