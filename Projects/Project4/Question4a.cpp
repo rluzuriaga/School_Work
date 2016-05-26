b02a7df236d24745550ac634dd13aec9151d6b42
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2016-05-26 12:30:32 -0700

    new file:   Projects/Project4/Answers/Q4a.exe 	deleted:    Projects/Project4/Answers/Q4b.exe 	modified:   Projects/Project4/Question4a.cpp 	new file:   Projects/Project4/files/file1 	new file:   Projects/Project4/files/file2 	new file:   Projects/Project4/files/file3

--- Projects/Project4/Question4a.cpp
+++ Projects/Project4/Question4a.cpp
@@ -6,10 +6,42 @@
  
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
+
+void FileUniter()
+{
+	//Variable declaration
+	ifstream file1, file2, file3;
+	ofstream united_file;
+	string text;
+	
+	file1.open("files/file1");
+	file2.open("files/file2");
+	file3.open("files/file3");
+	united_file.open("files/United_File");
+	
+	
+	getline(file1, text);
+	united_file << text << " ";
+	
+	getline(file2, text);
+	united_file << text << " ";
+	
+	getline(file3, text);
+	united_file << text << " \n";
+	
+	
+	file1.close();
+	file2.close();
+	file3.close();
+	united_file.close();
+	
+}
+
 main()
 {
-    
+	FileUniter();
 }
