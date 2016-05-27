08e283672a2b8a6c8982e1f3a6fff575c64a3aef
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2016-05-27 09:26:45 -0700

    modified:   Projects/Project4/Question4b.cpp 	new file:   Projects/Project4/a.out 	new file:   Projects/Project4/files/Fixed_Spaces_File 	new file:   Projects/Project4/files/United_File 	modified:   Projects/Project4/files/file1 	modified:   Projects/Project4/files/file2 	modified:   Projects/Project4/files/file3

--- Projects/Project4/Question4b.cpp
+++ Projects/Project4/Question4b.cpp
@@ -6,73 +6,129 @@
  
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-string Space_Corrector(string input)
+/*
+ * Two functions in this program to show how it works with files and also with user input.
+ */
+
+// SpaceCorrector_File()
+main()
 {
     //Variable declaration
-    int i, j = 1, size;
-    string output;
+    ifstream original_file;
+    ofstream fixed_file;
+    string text_holder_og, text_holder_modified;
+    int i, j = 1, text_size;
+    int counter = 0; // Only for testing.
+    string
     
-    size = input.size();
     
-    for (i = 0; i < size; i++)
+    original_file.open("files/United_File");
+    fixed_file.open("files/Fixed_Spaces_File");
+    
+    
+    getline(original_file, text_holder_og);
+    
+    text_size = text_holder_og.size();
+    
+    for  (i = 0; i < text_size; i++)
     {
-        if (input[i] == ' ')
+        if (text_holder_og[i] == ' ')
         {
-            if (input[j] != ' ')
+            if (text_holder_og[j] != ' ')
             {
-            	output = output + input[i];
+                text_holder_modified = text_holder_modified + text_holder_og[i];
+            }
+            else // Only for testing purposes.
+            {
+                counter++;
             }
         }
         else
         {
-            output = output + input[i];
+            text_holder_modified = text_holder_modified + text_holder_og[i];
         }
-        
         j++;
     }
     
-    return output;
+    
+    
+    cout << "OG text: " << endl << text_holder_og << endl << endl;
+    cout << "New text: " << endl << text_holder_modified << endl << endl;
+    cout << "This is the counter: " << counter << endl;
+    
+    original_file.close();
+    fixed_file.close();
 }
 
-main()
-{
-    //Variable declaration
-    string input;
-    string output;
-    int i, size, counter = 0;
+
+// string SpaceCorrector_cin(string input)
+// {
+//     //Variable declaration
+//     int i, j = 1, size;
+//     string output;
     
-    cout << "Please enter a string: ";
-    getline(cin, input);
+//     size = input.size();
     
-    size = input.size();
+//     for (i = 0; i < size; i++)
+//     {
+//         if (input[i] == ' ')
+//         {
+//             if (input[j] != ' ')
+//             {
+//             	output = output + input[i];
+//             }
+//         }
+//         else
+//         {
+//             output = output + input[i];
+//         }
+        
+//         j++;
+//     }
     
-    for (i = 0; i < size; i++)
-    {
-        if (input[i] == ' ')
-        {
-            counter++;
-        }
-    }
+//     return output;
+// }
+
+// main()
+// {
+//     //Variable declaration
+//     string input;
+//     string output;
+//     int i, size, counter = 0;
     
-    // cout << "This is how many spaces are in the string originally: " << counter << endl;
+//     cout << "Please enter a string: ";
+//     getline(cin, input);
     
-    output = Space_Corrector(input);
+//     size = input.size();
     
-    cout << "This is the string fixing the spaces: " << endl << output << endl;
+//     for (i = 0; i < size; i++)
+//     {
+//         if (input[i] == ' ')
+//         {
+//             counter++;
+//         }
+//     }
     
-    size = output.size();
-    counter = 0;
-    for (i = 0; i < size; i++)
-    {
-        if (output[i] == ' ')
-        {
-            counter++;
-        }
-    }
+//     // cout << "This is how many spaces are in the string originally: " << counter << endl;
     
-    // cout << "This is the fixed number of spaces from the output: " << counter << endl;
+//     output = Space_Corrector(input);
     
-}
+//     cout << "This is the string fixing the spaces: " << endl << output << endl;
+    
+//     size = output.size();
+//     counter = 0;
+//     for (i = 0; i < size; i++)
+//     {
+//         if (output[i] == ' ')
+//         {
+//             counter++;
+//         }
+//     }
+    
+//     // cout << "This is the fixed number of spaces from the output: " << counter << endl;
+    
+// }
